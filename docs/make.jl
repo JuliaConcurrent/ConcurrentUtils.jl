28f54af2df56a5d9f65075b0c197aee9c36f3e6c
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-28 01:32:26 -0400

    More checks in Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,20 @@ makedocs(
     sitename = "ConcurrentUtils",
     format = Documenter.HTML(),
     modules = [ConcurrentUtils],
+    strict = [
+        :autodocs_block,
+        :cross_references,
+        :docs_block,
+        :doctest,
+        :eval_block,
+        :example_block,
+        :footnote,
+        :linkcheck,
+        :meta_block,
+        # :missing_docs,
+        :parse_error,
+        :setup_block,
+    ],
     # Ref:
     # https://juliadocs.github.io/Documenter.jl/stable/lib/public/#Documenter.makedocs
 )
