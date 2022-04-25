adeea6175ea1b798d968708c0ff623ef16b38434
Takafumi Arakaki <aka.tkf@gmail.com>
2022-04-25 11:02:09 -0400

    Remove WriteLockHandle

--- src/lock_interface.jl
+++ src/lock_interface.jl
@@ -13,21 +13,13 @@ function ConcurrentUtils.acquire_read_then(f, lock::AbstractReadWriteLock)
     end
 end
 
-struct WriteLockHandle{RWLock} <: Base.AbstractLock
-    rwlock::RWLock
-end
-
 struct ReadLockHandle{RWLock} <: Base.AbstractLock
     rwlock::RWLock
 end
-
-Base.trylock(lck::WriteLockHandle) = trylock(lck.rwlock)
-Base.lock(lck::WriteLockHandle) = lock(lck.rwlock)
-Base.unlock(lck::WriteLockHandle) = unlock(lck.rwlock)
 
 Base.trylock(lck::ReadLockHandle) = Try.iok(try_race_acquire_read(lck.rwlock))
 Base.lock(lck::ReadLockHandle) = acquire_read(lck.rwlock)
 Base.unlock(lck::ReadLockHandle) = release_read(lck.rwlock)
 
 ConcurrentUtils.read_write_lock(lock::AbstractReadWriteLock = ReadWriteLock()) =
-    (ReadLockHandle(lock), WriteLockHandle(lock))
+    (ReadLockHandle(lock), lock)
